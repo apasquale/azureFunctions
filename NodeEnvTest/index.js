ce3027d0ba37127ca3fc1d65a4e2c77643f1ff25
Anthony Pasquale <apasquale@anzgcis.com>
2016-06-17 17:27:32 +1000

    Changed NodeEnvTest to be a sort of health check for environment vairables

--- NodeEnvTest/index.js
+++ NodeEnvTest/index.js
@@ -6,14 +6,9 @@ function GetEnvironmentVariable(name)
 module.exports = function(context, req) {
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
     context.log(GetEnvironmentVariable('APP_FOO'));
-    context.log(GetEnvironmentVariable('APP_Auth'));
-    context.log(process.env.app_auth);
-    context.log(GetEnvironmentVariable('APP_Slack_Token'));
-    var age = 3;
     
     context.res = {
-        // status: 400,
-        body: `Wohoo! I'm ${age} years old!`
+        body: `Confirming APP_FOO is: ${process.env.app_foo}`
     };
     
     context.done();
