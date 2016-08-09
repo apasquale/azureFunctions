f2b7341f1fff8928f3037f922cebf345c9324d18
Anthony Pasquale <apasquale@anzgcis.com>
2016-08-09 17:14:37 +1000

    Removed unneeded call

--- ChannelList/index.js
+++ ChannelList/index.js
@@ -1,6 +1,5 @@
 module.exports = function(context, req) {
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
-    context.log(GetEnvironmentVariable('APP_FOO'));
     
     context.res = {
         body: `We have the following channels:
