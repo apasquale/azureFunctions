6e357b9d60d9595b41cf853402ffa5a9ff339c51
Anthony Pasquale <apasquale@anzgcis.com>
2017-05-12 09:42:55 +1000

    Added a read message endpoint

--- SlackBot/index.js
+++ SlackBot/index.js
@@ -3,14 +3,14 @@ var https = require('https');
 var options = {
   host: 'slack.com',
   port: '443',
-  path: '/api/chat.postMessage?token='+process.env.app_auth_logn,
+  path: '/api/',
   method: 'GET'
 };
-//https://slack.com/api/chat.postMessage?&channel=C5CQ4HC6A&text=viapost&username=Pask%20Ghost&icon_emoji=%3Aghost%3A&pretty=1
 
 function postMessage(context, req) {
     
-    var message = '&channel=' + encodeURI(req.body.channel) + '&username=' + encodeURI(req.body.username) + '&text=' + encodeURI(req.body.message) + '&icon_emoji=' + encodeURI(req.body.emoji);
+    var message = '/api/chat.postMessage?token=' + process.env.app_auth_logn + '&channel=' + encodeURI(req.body.channel) + '&username=' + encodeURI(req.body.username) + '&text=' + encodeURI(req.body.message) + '&icon_emoji=' + encodeURI(req.body.emoji);
+
     options.path += message
 
     context.log('Responding with: ', message);
@@ -34,9 +34,47 @@ function postMessage(context, req) {
     slackSend.end();
 }
 
+function readMessage(context, req) {
+    
+    var message = '/api/channels.history?token=' + process.env.app_auth_logn + '&channel=' + encodeURI(req.body.channel) + '&count=' + req.body.count;
+    options.path += message
+
+    context.log('Responding with: ', message);
+    context.log('Options: ', options);
+
+    var slackSend = https.request(options, function(res) {
+        var msg = '';
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            msg += chunk;
+        });
+        res.on('end', function() {
+            var myresult = JSON.parse(msg);
+    
+            context.res = {
+                body: myresult
+            };
+            context.done();
+        });
+    });
+    slackSend.end();
+}
 
 module.exports = function(context, req) {
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
 
-    postMessage(context, req);
+    switch (req.body.command)
+    {
+        case "post":
+            postMessage(context, req);
+            break;
+        case "read":
+            readMessage(context, terms);
+            break;default: 
+            context.res = {
+                status: 400,
+                body: `Incorrect function: ${command} Only 'list', 'search' and 'get' are supported at this stage`
+            };
+            context.done();
+    }
 };
